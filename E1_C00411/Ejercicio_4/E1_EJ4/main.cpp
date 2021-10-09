18bae6c063a6a754769838a6defa3899daa07f8f
Eiran <eiran.amadoralvarado@gmail.com>
2021-10-08 18:11:36 -0600

    Actualizan datos iniciales para los ejercicio

---
+++ E1_C00411/Ejercicio_4/E1_EJ4/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "calculadoraCosto.h"
+
+int main() {
+
+    CalculadoraCosto calculadora {2};
+
+    float costoFinal = calculadora.CalcularCosto(5);
+    std::cout << costoFinal;
+
+    return 0;
+}
